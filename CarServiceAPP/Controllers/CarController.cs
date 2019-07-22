6d7d17958865c37894edf3c9e7ab22503efa8ef7
Ashish Aswani <ashishaswani0708@gmail.com>
2019-07-22 21:38:28 +0530

    Updated

--- CarServiceAPP/Controllers/CarController.cs
+++ CarServiceAPP/Controllers/CarController.cs
@@ -41,24 +41,46 @@ namespace CarServiceAPP.Controllers
 
 		public ActionResult AddCar(CarCustomer carCustomer)
 		{
-           
-            //if (carCustomer.Cars.Id == 0)
-            //{
+
+            if (carCustomer.Cars.Id == 0)
+            {
                 carCustomer.Cars.CustomerId = carCustomer.Customers.Id;
                 _context.Cars.Add(carCustomer.Cars);
-            //}
-            //else
-            //{
-            //    var carInDb = _context.Cars.Single(c => c.Id == carCustomer.Cars.Id);
-            //    carInDb.VIN = carCustomer.Cars.VIN;
-            //    carInDb.Make = carCustomer.Cars.Make;
-            //    carInDb.Model = carCustomer.Cars.Model;
-            //    carInDb.Style = carCustomer.Cars.Style;
-            //    carInDb.Year = carCustomer.Cars.Year;
-            //}
-			_context.SaveChanges();
+            }
+            else
+            {
+                var carInDb = _context.Cars.Single(c => c.Id == carCustomer.Cars.Id);
+                carInDb.VIN = carCustomer.Cars.VIN;
+                carInDb.Make = carCustomer.Cars.Make;
+                carInDb.Model = carCustomer.Cars.Model;
+                carInDb.Style = carCustomer.Cars.Style;
+                carInDb.Year = carCustomer.Cars.Year;
+            }
+            _context.SaveChanges();
 			return RedirectToAction("ViewDetails", "Customer",carCustomer.Customers);
             
 		}
+
+        public ActionResult EditCar(CarCustomer carCustomer)
+        {
+            var editCar = _context.Cars.Find(carCustomer.Cars.Id);
+            if (carCustomer == null)
+                return HttpNotFound();
+            else
+            {
+                carCustomer.Cars = editCar;
+                return View("CarForm", carCustomer);
+            }
+        }
+
+        public ActionResult DeleteCar(Car car)
+        {
+            var deleteCar = _context.Cars.Find(car.Id);
+            var id = car.CustomerId;
+            _context.Cars.Remove(deleteCar);
+            _context.SaveChanges();
+            var customer = _context.Customers.Find(id);
+            return RedirectToAction("ViewDetails", "Customer",customer);
+        }
     }
 }
