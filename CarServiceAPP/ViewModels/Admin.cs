900435fc0da4f142c576089661f5d9bbde66131a
Ashish Aswani <ashishaswani0708@gmail.com>
2019-08-05 14:35:39 +0530

    Admin Section needs to be done

---
+++ CarServiceAPP/ViewModels/Admin.cs
@@ -0,0 +1,17 @@
+﻿using CarServiceAPP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarServiceAPP.ViewModels
+{
+    public class Admin
+    {
+        public IEnumerable<CarMake> CarMakes { get; set; }]
+
+        public IEnumerable<CarStyle> CarStyles { get; set; }
+
+        public IEnumerable<ServiceType> ServiceTypes { get; set; }
+    }
+}
