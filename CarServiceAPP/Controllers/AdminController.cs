d1cd0826b4c17a6cd4fbbf60a550a03a2fc3c2c0
Ashish Aswani <ashishaswani0708@gmail.com>
2019-08-06 18:48:21 +0530

    Administrator Services done

--- CarServiceAPP/Controllers/AdminController.cs
+++ CarServiceAPP/Controllers/AdminController.cs
@@ -1,22 +1,80 @@
-﻿using System;
+﻿using CarServiceAPP.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CarServiceAPP.ViewModels;
 
 namespace CarServiceAPP.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin
-        public ActionResult Index()
+        ApplicationDbContext _context;
+
+        public AdminController()
         {
-            return View();
+            _context = new ApplicationDbContext();
         }
 
-        public ActionResult AdminServices()
+        protected override void Dispose(bool disposing)
         {
-            return View();
+            _context.Dispose();
+        }
+        
+        public ActionResult Index()
+        {
+            var viewModel = new Admin
+            {
+                CarMakes = _context.CarMakes.ToList(),
+                CarStyles = _context.CarStyles.ToList(),
+                ServiceTypes = _context.ServiceTypes.ToList()
+            };
+            return View(viewModel);
+        }
+
+        public ActionResult AddMake(Admin admin)
+        {
+            _context.CarMakes.Add(admin.CarMake);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Admin");
+        }
+
+        public ActionResult DeleteMake(int? id)
+        {
+            var deleteMake = _context.CarMakes.Find(id);
+            _context.CarMakes.Remove(deleteMake);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Admin");
+        }
+
+
+        public ActionResult AddStyle(Admin admin)
+        {
+            _context.CarStyles.Add(admin.CarStyle);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Admin");
+        }
+        public ActionResult DeleteStyle(int? id)
+        {
+            var deleteStyle = _context.CarStyles.Find(id);
+            _context.CarStyles.Remove(deleteStyle);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Admin");
+        }
+
+        public ActionResult AddServiceType(Admin admin)
+        {
+            _context.ServiceTypes.Add(admin.ServiceType);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Admin");
+        }
+        public ActionResult DeleteServiceType(int? id)
+        {
+            var deleteServiceType = _context.ServiceTypes.Find(id);
+            _context.ServiceTypes.Remove(deleteServiceType);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Admin");
         }
     }
 }
--- CarServiceAPP/ViewModels/Admin.cs
+++ CarServiceAPP/ViewModels/Admin.cs
@@ -8,10 +8,14 @@ namespace CarServiceAPP.ViewModels
 {
     public class Admin
     {
-        public IEnumerable<CarMake> CarMakes { get; set; }]
+        public IEnumerable<CarMake> CarMakes { get; set; }
+        public CarMake CarMake { get; set; }
 
         public IEnumerable<CarStyle> CarStyles { get; set; }
+        public CarStyle CarStyle { get; set; }
 
         public IEnumerable<ServiceType> ServiceTypes { get; set; }
+        public ServiceType ServiceType { get; set; }
+
     }
 }
